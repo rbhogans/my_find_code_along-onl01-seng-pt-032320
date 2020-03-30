36b9c33784110ff1d4df4861795ed490d563f966
Rabiah Hogans <hogansrabiah@gmail.com>
2020-03-30 23:22:48 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -12,3 +12,13 @@ def my_find(collection)
     i = i + 1
   end
 end
+
+def my_find(collection)
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    end
+    i = i + 1
+  end
+end
